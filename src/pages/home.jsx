a52752ee0c267f8d0bf7f4eb558ab6fcda646f26
nicholasmendoza140 <nicholasmendoza140@gmail.com>
2026-04-06 17:35:22 -0700

    teams on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,30 @@
-
+import { useEffect, useState } from "react"
+import api from "../api"
 
 export default function Home() {
     
+    const [teams, setTeams] = useState([])
+
+    useEffect(() => {
+        getTeams()
+    }, [])
+    const getTeams = async () => {
+        try {
+            const res = await api.get("/teams")
+            setTeams(res.data)
+        } catch (err) {
+            console.error(err)
+            alert("Failed to get teams")
+        }
+
+    }
 
     return (
         <div>
             <h2>Home</h2>
+            {teams.map((t) => {
+                return <div key={t.id}>{t.name}</div>
+            })}
         </div>
     )
 }
